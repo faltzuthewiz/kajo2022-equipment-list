8f1201dd7300eeb46865c2dcbabb5436ec377e57
Sonja Fallstrom <jawnlemon@gmail.com>
2022-06-22 21:45:26 +0300

    added Swe, fixed small details

---
+++ equipment_list_swe.js
@@ -0,0 +1,26 @@
+var i, checkboxes = document.querySelectorAll(".EQUIP-check");
+
+function save() {
+    for (i = 0; i < checkboxes.length; i++) {
+        localStorage.setItem(checkboxes[i].value, checkboxes[i].checked);
+    }
+    const btnCollection = document.getElementsByClassName("savebtn");
+    for (let i = 0; i < btnCollection.length; i++) {
+        btnCollection[i].textContent = "Sparat!";
+    }
+    //document.getElementsByClassName("#savebtn").textContent = "Tallennettu!";
+    setTimeout(timeOut, 3000);
+}
+
+function timeOut() {
+    const btnCollection = document.getElementsByClassName("savebtn");
+    for (let i = 0; i < btnCollection.length; i++) {
+        btnCollection[i].textContent = "Spara";
+    }
+}
+
+function load_() {
+    for (i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = localStorage.getItem(checkboxes[i].value) === 'true' ? true:false;
+    }
+}
